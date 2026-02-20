37319dd25f64e25d777d54ed777fdf1bb5793d57
annieouseph8 <annieouseph872@gmail.com>
2026-02-20 23:44:48 +0530

    deleted serializer as django auth model is used

--- user_sys_be/user_be_app/serializer.py
+++
@@ -1,7 +0,0 @@
-from django.core import serializers
-
-class UserSerializer(serializers.ModelSerialize):
-    class Meta:
-        model = User #which model to serialize
-        fields = '__all__' # what all fields to include
-        
